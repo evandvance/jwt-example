0e980329d305ee8af0d32cc73cb38ca1d59c1c5a
Evan <evandvance@gmail.com>
2024-04-02 14:20:52 -0500

    Added login and creation endpoints

--- api/routes/Users.ts
+++ api/routes/Users.ts
@@ -1,16 +1,41 @@
 import router, { Response, Request } from 'express';
-import { getUser } from '../services/UserService';
+import { getUser, makeUser } from '../services/UserService';
+import { generateToken } from '../auth/auth';
+import bcrypt from 'bcrypt';
 
 const SALT_ROUNDS = 8;
 
 const userRoute = router();
 
-userRoute.get('/login', async (req: Request, res: Response) => {
+userRoute.post('/login', async (req: Request, res: Response) => {
   const { email, password } = req.body;
+
+  if (email === null || password === null) {
+    res.status(400).json({ message: 'Request missing email or password' });
+  }
 
   const user = await getUser(email);
 
-  if (user!.password == bcrypt.hash) res.send();
+  if (user!.password == (await bcrypt.hash(password, SALT_ROUNDS))) {
+    res.json({ jwt: generateToken(email) });
+  } else {
+    res.send('Login Failed');
+  }
 });
 
+userRoute.post('/', async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email === null || password === null) {
+    res.status(400).json({ message: 'Request missing email or password' });
+  }
+
+  try {
+    const newUser = await makeUser(email, password);
+
+    res.json(newUser);
+  } catch {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 export default userRoute;
