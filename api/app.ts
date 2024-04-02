39906f944c6d9715dd40ee50b9f53010a3ced5ab
Evan <evandvance@gmail.com>
2024-04-02 14:22:12 -0500

    Registered routes with app

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,13 @@
 import express, { Express } from 'express';
+import userRoute from './routes/Users';
+import contentRoute from './routes/ProtectedContent';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use('/users', userRoute);
+app.use('/content', contentRoute);
 
 app.listen(() => {
   console.log(`App is listening on port ${PORT}`);
